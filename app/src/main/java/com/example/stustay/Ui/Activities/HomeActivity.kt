0a19cb90c698e14f61e3226e8bece8f8f79475c0
Yassine Ezzar <yassine.ezzar@esprit.tn>
2023-12-10 11:29:09 +0100

    stustay thdm

--- app/src/main/java/com/example/stustay/Ui/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/stustay/Ui/Activities/HomeActivity.kt
@@ -3,6 +3,9 @@ package com.example.stustay.Ui.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.stustay.R
@@ -30,6 +33,55 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        setupSearchBar()
+        binding.btnOrderNow1.setOnClickListener {
+            val intent = Intent(this, DetailsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    private fun setupSearchBar() {
+        binding.searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed for this example
+            }
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed for this example
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Filter data based on the search query
+                val searchQuery = s.toString().trim()
+                filterData(searchQuery)
+            }
+        })
+    }
+
+    private fun filterData(searchQuery: String) {
+        // Check the search query and show/hide card views accordingly
+        when {
+            searchQuery.equals("Ariana", ignoreCase = true) -> {
+                binding.cardView1.visibility = View.VISIBLE
+                binding.cardView2.visibility = View.GONE
+                binding.cardView3.visibility = View.GONE
+            }
+            searchQuery.equals("Studio", ignoreCase = true) -> {
+                binding.cardView1.visibility = View.GONE
+                binding.cardView2.visibility = View.VISIBLE
+                binding.cardView3.visibility = View.GONE
+            }
+            searchQuery.equals("Mourouj", ignoreCase = true) -> {
+                binding.cardView1.visibility = View.GONE
+                binding.cardView2.visibility = View.GONE
+                binding.cardView3.visibility = View.VISIBLE
+            }
+            else -> {
+                // If no match, show all card views
+                binding.cardView1.visibility = View.VISIBLE
+                binding.cardView2.visibility = View.VISIBLE
+                binding.cardView3.visibility = View.VISIBLE
+            }
+        }
     }
 }
